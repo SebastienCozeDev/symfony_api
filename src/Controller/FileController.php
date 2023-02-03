a140c02e153c8ba314d8046ea7f4c560017c9b16
Coze Sebastien <sebastien_coze@ens.univ-artois.fr>
2023-02-03 19:30:09 +0100

    Ajout de la réception du fichier et de sa correction.

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -3,16 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/file')]
 class FileController extends AbstractController
 {
-    #[Route('/file', name: 'app_file')]
-    public function index(): Response
+    /**
+     * @throws \Exception
+     */
+    #[Route("/{mode}", name: "file_process", methods: "POST")]
+    public function addFile(Request $request, string $mode): JsonResponse
     {
-        return $this->render('file/index.html.twig', [
-            'controller_name' => 'FileController',
-        ]);
+        $file = $request->files->get('file');
+
+        // Obtenir le contenu du fichier qui a été upload.
+        $content = file_get_contents($file->getPathname());
+
+        // Application la correction souhaitée.
+        if ($mode === 'upper') {
+            $content = strtoupper($content);
+        } elseif ($mode === 'lower') {
+            $content = strtolower($content);
+        } else {
+            throw new \Exception('Mode de correction invalide');
+        }
+        return new JsonResponse(['content' => $content]);
     }
 }
--- src/Entity/File.php
+++ src/Entity/File.php
@@ -4,8 +4,10 @@ namespace App\Entity;
 
 use App\Repository\FileRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: FileRepository::class)]
+#[ApiResource]
 class File
 {
     #[ORM\Id]
